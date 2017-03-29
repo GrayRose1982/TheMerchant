dff671a9ccc9c26c1dd54fbc15beb72711bf9894
Hoàng Đức Long <nothing.noonedr@gmail.com>
2017-03-29 15:36:20 +0700

    FirstCommit

---
+++ Assets/Scripts/BaseInformation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseInformation : BaseStar
+{
+	public string index;
+	public string name;
+	public string description;
+
+	public void DoSomething ()
+	{
+	}
+}
---
+++ Assets/Scripts/BaseStar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BaseStar
+{
+	protected int _str;
+	protected int _agi;
+	protected int _int;
+
+	public void Test ()
+	{
+	}
+}
